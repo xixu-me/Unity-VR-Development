69627cbb57a34cb03096c79599faf7caa0415c88
Xi Xu <i@xi-xu.me>
2025-02-21 11:01:15 +0800

    Update Unity project scene and library files

---
+++ HelloCube/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Bullet: Start");
+		Destroy (this.gameObject, 3f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ HelloCube/Assets/Scripts/Shooter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+	public Rigidbody bullet;
+	public float power = 1500f;
+	public float moveSpeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Shooter: Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal") * Time.deltaTime * moveSpeed;
+		float v = Input.GetAxis ("Vertical") * Time.deltaTime * moveSpeed;
+
+		this.transform.Translate (h, v, 0);
+
+		if (Input.GetButtonUp ("Fire1")) {
+			Rigidbody instance = Instantiate (bullet,
+			                                  this.transform.position,
+			                                  this.transform.rotation) as Rigidbody;
+			Vector3 forward = this.transform.TransformDirection(
+				Vector3.forward);
+			instance.AddForce (forward * power);
+		}
+	}
+}
