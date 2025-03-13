388161e953d208a2589d224c99bb4b65488d79d8
Xi Xu <i@xi-xu.me>
2025-03-13 20:23:55 +0800

    Update SurvivalIsland

--- SurvivalIsland/Assets/Scripts/PlayerCollisions.cs
+++ SurvivalIsland/Assets/Scripts/PlayerCollisions.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class PlayerCollisions : MonoBehaviour {
-	bool doorIsOpen = false;
-	float doorTimer = 0.0f;
-	public float doorOpenTime = 3.0f;
-	public AudioClip doorOpenSound;
-	public AudioClip doorShutSound;
+	// bool doorIsOpen = false;
+	// float doorTimer = 0.0f;
+	// public float doorOpenTime = 3.0f;
+	// public AudioClip doorOpenSound;
+	// public AudioClip doorShutSound;
 
 	GameObject currentDoor;
 
@@ -17,26 +17,33 @@ public class PlayerCollisions : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (doorIsOpen) {
-			doorTimer += Time.deltaTime;
-
-			if (doorTimer > doorOpenTime) {
-				Door (doorShutSound, false, "doorshut",currentDoor);
-				doorTimer = 0.0f;
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, 3)) {
+			if (hit.collider.gameObject.tag == "playerDoor") {
+				currentDoor = hit.collider.gameObject;
+				currentDoor.SendMessage ("DoorCheck");
 			}
 		}
+		// if (doorIsOpen) {
+		//	 doorTimer += Time.deltaTime;
+
+		// if (doorTimer > doorOpenTime) {
+		//		Door (doorShutSound, false, "doorshut",currentDoor);
+		//		doorTimer = 0.0f;
+		//	 }
+		// }
 	}
 
-	void OnControllerColliderHit(ControllerColliderHit hit) {
-		if (hit.gameObject.tag == "playerDoor" && doorIsOpen == false) {
-			currentDoor = hit.gameObject;
-			Door (doorOpenSound, true, "dooropen",currentDoor);
-		}
-	}
+	// void OnControllerColliderHit(ControllerColliderHit hit) {
+	//	 if (hit.gameObject.tag == "playerDoor" && doorIsOpen == false) {
+	//		 currentDoor = hit.gameObject;
+	//		 Door (doorOpenSound, true, "dooropen",currentDoor);
+	//	 }
+	// }
 
-	void Door(AudioClip aClip, bool openCheck, string animName,GameObject thisDoor) {
-		thisDoor.audio.PlayOneShot (aClip);
-		doorIsOpen = openCheck;
-		thisDoor.transform.parent.animation.Play (animName);
-	}
+	// void Door(AudioClip aClip, bool openCheck, string animName,GameObject thisDoor) {
+	//	 thisDoor.audio.PlayOneShot (aClip);
+	//	 doorIsOpen = openCheck;
+	//	 thisDoor.transform.parent.animation.Play (animName);
+	// }
 }
---
+++ SurvivalIsland/Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerZone : MonoBehaviour
+{
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			transform.FindChild("door").SendMessage("DoorCheck");
+		}
+	}
+}
